64d68c0c98d03d97f552fbcc82aa15bb275c7797
juLia <84445011+ttpmorp@users.noreply.github.com>
2025-01-30 09:44:56 -0300

    Add files via upload

---
+++ Projeto01/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using System.Linq.Expressions;
+using Projeto01.Entities;
+using Projeto01.Repositories;
+
+namespace Projeto01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //IMPRIMIR MENSAGEM(CONSOLE DO DOS)
+            Console.WriteLine("\n *** CADASTRO DE CLIENTES *** \n");
+
+            var cliente = new Cliente();
+            cliente.Id = Guid.NewGuid();
+
+
+            try
+            {
+                Console.WriteLine("Informe o NOME do cliente: ");
+                cliente.Nome = Console.ReadLine();
+
+                Console.WriteLine("Informe o CPF do cliente: ");
+                cliente.Cpf = Console.ReadLine();
+
+                Console.WriteLine("Informe o EMAIL do cliente: ");
+                cliente.Email = Console.ReadLine();
+
+                Console.WriteLine("Informe a DATA DE NASCIMENTO do cliente: ");
+                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+
+                var clienteRepository = new ClienteRepository();
+                clienteRepository.GravarArquivo(cliente);
+
+
+                Console.WriteLine("\nCLIENTE CADASTRADO COM SUCESSO!");
+                Console.WriteLine("\nDADOS DO CLIENTE: ");
+                Console.WriteLine($"ID do cliente: {cliente.Id}");
+                Console.WriteLine($"nome:  {cliente.Nome}");
+                Console.WriteLine($"CPF: {cliente.Cpf}");
+                Console.WriteLine($"Email: {cliente.Email}");
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n FALHA AO CADASTRAR O CLIENTE!");
+                Console.WriteLine($"ERRO:{e.Message}");
+            
+            } 
+
+
+            Console.WriteLine($"Data de Nascimento: {cliente.DataNascimento}");
+
+            //PAUSAR O PROMPT DE COMANDOS
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ Projeto01/Repositories/ClienteRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto01.Entities;
+
+namespace Projeto01.Repositories
+{
+    public class ClienteRepository
+    {
+        public void GravarArquivo(Cliente cliente)
+
+        {
+            var path = $"G:\\COTI\\PROJETOS\\Projeto01\\temp\\cliente_{cliente.Id}.txt";
+
+            var streamWriter = new StreamWriter(path);
+
+            streamWriter.WriteLine($"ID:{cliente.Id}");
+            streamWriter.WriteLine($"NOME:{cliente.Nome}");
+            streamWriter.WriteLine($"CPF:{cliente.Cpf}");
+            streamWriter.WriteLine($"EMAIL:{cliente.Email}");
+            streamWriter.WriteLine($"DATA DE NASCIMENTO:{cliente.DataNascimento}");
+
+
+            streamWriter.Flush();
+            streamWriter.Close();
+        }
+    }
+}
